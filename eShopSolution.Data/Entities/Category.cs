e14936beabe142ee818fcf763d12f99c917e3e98
Phạm Trung Anh Dũng <73769984+phamtrunganhdung@users.noreply.github.com>
2023-02-04 07:04:33 +0700

    setup entities

--- eShopSolution.Data/Entities/Category.cs
+++ eShopSolution.Data/Entities/Category.cs
@@ -1,18 +1,21 @@
 ﻿using eShopSolution.Data.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace eShopSolution.Data.Entities
 {
     public class Category
     {
-            public int Id { get; set; }
-            public int SortOrder { get; set; }
-            public bool IsShowOnHome { get; set; }
-            public int? ParentId { get; set; }
-            public Status Status { get; set; }
-    }           
+        public int Id { set; get; }
+        public int SortOrder { set; get; }
+        public bool IsShowOnHome { set; get; }
+        public int? ParentId { set; get; }
+        public Status Status { set; get; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<CategoryTranslation> CategoryTranslations { get; set; }
+
+    }
 }
